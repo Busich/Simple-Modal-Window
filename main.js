aab7948cd4d553b4d607014feb160cec50e706e8
Busich <busich.png@gmail.com>
2024-02-08 17:58:59 +0200

    modal-window

---
+++ main.js
@@ -0,0 +1,41 @@
+function bindModal(triggerSelector, modalSelector, closeSelector) {
+  const trigger = document.querySelectorAll(triggerSelector),
+    modal = document.querySelector(modalSelector),
+    close = document.querySelector(closeSelector);
+
+  trigger.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (e.target) {
+        e.preventDefault();
+      }
+
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden";
+      // document.body.classList.add('modal-open');
+    });
+  });
+
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+    // document.body.classList.remove('modal-open');
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+      // document.body.classList.remove('modal-open');
+    }
+  });
+}
+
+function showModalByTime(selector, time) {
+  setTimeout(function () {
+    document.querySelector(selector).style.display = "block";
+    document.body.style.overflow = "hidden";
+  }, time);
+}
+
+bindModal(".btn-success", "#myModal", ".modal-content .close");
+// showModalByTime('.popup', 60000);
